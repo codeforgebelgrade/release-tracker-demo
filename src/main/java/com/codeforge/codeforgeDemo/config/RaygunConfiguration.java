02341894c2da063e2cd3f14b2f878243fd97edd8
Miroslav Lazovic <miroslav.lazovic@finiti.rs>
2021-11-01 10:43:19 +0100

    Raygun implementation added

---
+++ src/main/java/com/codeforge/codeforgeDemo/config/RaygunConfiguration.java
@@ -0,0 +1,31 @@
+package com.codeforge.codeforgeDemo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration()
+@ConfigurationProperties(prefix = "raygun")
+public class RaygunConfiguration {
+
+    private boolean enabled;
+    private String clientId;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+
+
+}
--- src/main/java/com/codeforge/codeforgeDemo/global/GlobalControllerExceptionHandler.java
+++ src/main/java/com/codeforge/codeforgeDemo/global/GlobalControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package com.codeforge.codeforgeDemo.global;
 import com.codeforge.codeforgeDemo.global.exception.EntityNotFoundException;
 import com.codeforge.codeforgeDemo.global.exception.ParameterValidationException;
 import com.codeforge.codeforgeDemo.model.api.ApiResponse;
+import com.mindscapehq.raygun4java.core.RaygunClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,32 +17,60 @@ public class GlobalControllerExceptionHandler {
 
     private static final Logger logger = LogManager.getLogger(GlobalControllerExceptionHandler.class);
 
+    @Value("${raygun.clientId}")
+    private String raygunClientId;
+
+    @Value("${raygun.enabled}")
+    private boolean raygunEnabled;
+
+    private RaygunClient raygunClient = null;
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleException(Exception exception){
         if(exception instanceof ParameterValidationException) {
-            logger.error("ParameterValidationException occured. Message: {}", exception.getMessage());
+            logger.error("ParameterValidationException occurred. Message: {}", exception.getMessage());
+            if(raygunEnabled) {
+                this.logRaygunEvent(exception);
+            }
             ApiResponse response = new ApiResponse(GlobalConstants.API_RESULT_ERROR);
             response.setDescription(exception.getMessage());
             return ResponseEntity.badRequest().body(response);
 
         } else if (exception instanceof EntityNotFoundException) {
-            logger.error("EntityNotFoundException occured. Message: {}", exception.getMessage());
+            logger.error("EntityNotFoundException occurred. Message: {}", exception.getMessage());
+            if(raygunEnabled) {
+                this.logRaygunEvent(exception);
+            }
             ApiResponse response = new ApiResponse(GlobalConstants.API_RESULT_ERROR);
             response.setDescription(exception.getMessage());
             return ResponseEntity.notFound().build();
 
         } else if (exception instanceof MethodArgumentTypeMismatchException) {
-            logger.error("MethodArgumentTypeMismatchException occured. Message: {}", exception.getMessage());
+            logger.error("MethodArgumentTypeMismatchException occurred. Message: {}", exception.getMessage());
+            if(raygunEnabled) {
+                this.logRaygunEvent(exception);
+            }
             ApiResponse response = new ApiResponse(GlobalConstants.API_RESULT_ERROR);
             response.setDescription(exception.getMessage());
             return ResponseEntity.badRequest().body(response);
 
         } else {
-            logger.error("Exception occured. Message: {}", exception.getMessage());
+            logger.error("Exception occurred. Message: {}", exception.getMessage());
+            if(raygunEnabled) {
+                this.logRaygunEvent(exception);
+            }
             exception.printStackTrace();
             ApiResponse response = new ApiResponse(GlobalConstants.API_RESULT_ERROR);
             response.setDescription(exception.getMessage());
             return ResponseEntity.internalServerError().body(response);
         }
+    }
+
+    private void logRaygunEvent(Exception ex) {
+
+        if(raygunClient == null) {
+            raygunClient = new RaygunClient(raygunClientId);
+        }
+        raygunClient.send(ex);
     }
 }
